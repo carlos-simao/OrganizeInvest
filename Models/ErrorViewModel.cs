3a0228fddc00c1bb334afab10bc14420d5a03291
Carlos Simão <carlosgomes8127@outlook.com.br>
2020-06-24 14:29:51 -0300

    Criação do projeto e implementação do templante com a primeira Deshbord

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrganizeInvest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
